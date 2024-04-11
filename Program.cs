4055e63eda1fed42889f95c4a4bb3e2d2cd18378
Danillo <danillordm@gmail.com>
2024-04-11 10:13:38 -0300

    Add Outros/Interpolacao

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using csharp_lembretes.Tipos_primitivos;
+﻿using csharp_lembretes.Outros;
+using csharp_lembretes.Tipos_primitivos;
 
 TiposPrimitivos tiposPrimitivos = new TiposPrimitivos('a');
 tiposPrimitivos.boleano = true;
+
+Interpolacao interpolacao = new Interpolacao("Exemplo");
