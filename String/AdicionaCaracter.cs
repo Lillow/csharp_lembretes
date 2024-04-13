0cc471fe8538e1cc1f13752741e88a0b0283627c
Danillo <danillordm@gmail.com>
2024-04-12 21:49:58 -0300

    Add String

---
+++ String/AdicionaCaracter.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace csharp_lembretes.String
+{
+    internal class AdicionaCaracter
+    {
+
+        public string AdicionaCaracterEsquerda( int numeroCaractere, char caractere)
+        {
+            return string.Empty.PadLeft(numeroCaractere, caractere);
+        }
+
+        public string AdicionaCaracterDireita(int numeroCaractere, char caractere)
+        {
+            return  string.Empty.PadRight(numeroCaractere, caractere);
+        }
+
+    }
+}
