41873cb8ec89d6e5b64b290021c41ea7dca2fa41
Danillo <danillordm@gmail.com>
2024-04-17 16:34:00 -0300

    Add DataTempo

---
+++ csharp_lembretes/Variaveis_tipos_de_dados/DataTempo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Metadata;
+
+namespace csharp_lembretes.Variaveis_tipos_de_dados
+{
+    internal class DataTempo
+    {
+        public DataTempo() 
+        { 
+             DateTime dataAtual = DateTime.Now;
+             DateTime dataAtualMais5dias = DateTime.Now.AddDays(5);
+
+            Console.WriteLine(dataAtual.ToString("dd/MMMM/yyyy HH:mm"));
+        
+        
+        }
+
+        
+
+    }
+}
