093c02bccbc362c51c53ea9476cbe5191fc964cf
Danillo <danillordm@gmail.com>
2024-04-21 13:35:31 -0300

    Add Bibliotecas

---
+++ csharp_lembretes/Bibliotecas/OpenAI/OpenAI_.cs
@@ -0,0 +1,19 @@
+﻿namespace csharp_lembretes.Bibliotecas.OpenAI;
+
+using OpenAI_API;
+internal class OpenAI_
+{
+    public async void OpenAI()
+    {
+
+        var cliente = new OpenAIAPI("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");
+
+        var chat = cliente.Chat.CreateConversation();
+
+        chat.AppendSystemMessage("Resuma a banda Linkin Park em um parágrafo.");
+
+        string resposta = await chat.GetResponseFromChatbotAsync();
+
+        Console.WriteLine(resposta);
+    }
+}
