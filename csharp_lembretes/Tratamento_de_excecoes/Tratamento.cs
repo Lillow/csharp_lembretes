b66a30433c8d6b30f18ce62abd703a9893a5b16c
Danillo <danillordm@gmail.com>
2024-04-23 10:34:05 -0300

    Adiciona Tratamento_de_excecoes

---
+++ csharp_lembretes/Tratamento_de_excecoes/Tratamento.cs
@@ -0,0 +1,18 @@
+﻿namespace csharp_lembretes.Tratamento_de_excecoes;
+
+internal class Tratamento
+{
+    public void Teste()
+    {
+        try
+        {
+            //Expressão a ser testada
+
+        }
+        catch (Exception ex)
+        {
+            // Se cair na excessão
+            Console.WriteLine($"Temos um problema: {ex.Message}");
+        }
+    }
+}
