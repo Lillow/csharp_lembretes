ecddea428358b0700d4a51ff1c5b1716649b32ff
Danillo <danillordm@gmail.com>
2024-04-25 19:10:53 -0300

    Adiciona informações em Array.cs

--- csharp_lembretes/Colecoes/Arrays.cs
+++ csharp_lembretes/Colecoes/Arrays.cs
@@ -1,32 +1,58 @@
-﻿namespace csharp_lembretes.Colecoes
+﻿namespace csharp_lembretes.Colecoes;
+
+internal class Arrays
 {
-    internal class Arrays
+    public static void Vetor()
     {
-        public void Vetor()
+        int[] numeros = new int[5]; // Declarando e inicializando array de 5 posições
+
+        int[] initArray = [1, 2, 3, 4]; // Declarando e inicializando array
+
+        initArray[0] = 2; // Trocando o valor da posição 0
+
+        Console.WriteLine($"Tamanho do array:  {numeros.Length}"); // Tabanho do array
+    }
+
+    public static void Matriz()
+    {
+
+        int[,] multiDimensionalArray1 = new int[2, 3]; // Declara Array com duas dimenções
+
+        Console.WriteLine(multiDimensionalArray1.Length);
+
+        int[,] multiDimensionalArray2 = { { 1, 2, 3 }, { 4, 5, 6 } };// Declarando e inicializando array com duas dimenções
+
+        Console.WriteLine(multiDimensionalArray2.Length);
+
+        int[][] jaggedArray = new int[6][]; // Declara um array irregular
+
+        // Defina os valores do primeiro array na estrutura de array irregular
+        jaggedArray[0] = [1, 2, 3, 4];
+
+    }
+
+    public static void RedimencionarArray(int[] array) 
+    {
+        Console.WriteLine($"Tamanho do array:  {array.Length}");
+
+        Array.Resize(ref array, array.Length * 2); // Redimenciona o Array
+
+        Console.WriteLine($"Tamanho do array depois do Resize:  {array.Length}");
+    }
+
+    public static void CopiarArray(int[] array)
+    {
+        Console.WriteLine("Array:");
+        foreach (int i in array)
         {
-            int[] numeros = new int[5]; // Declarando e inicializando array de 5 posições
-
-            int[] initArray = { 1, 2, 3, 4 }; // Declarando e inicializando array
-
-            initArray[0] = 2; // Trocando o valor da posição 0
-
-            Console.WriteLine(numeros.Length); // Tabanho do array
-
-        }
-
-        public void Matriz()
+            Console.WriteLine(i);
+        } 
+        int[] arrayCopia = new int[array.Length * 2];
+        Array.Copy(array, arrayCopia, array.Length); // Copia um array para um outro
+        Console.WriteLine("Array cópia:");
+        foreach (int i in array)
         {
-
-            int[,] multiDimensionalArray1 = new int[2, 3]; // Declara Array com duas dimenções
-
-            int[,] multiDimensionalArray2 = { { 1, 2, 3 }, { 4, 5, 6 } };// Declarando e inicializando array com duas dimenções
-
-
-            int[][] jaggedArray = new int[6][]; // Declara um array irregular
-
-            // Defina os valores do primeiro array na estrutura de array irregular
-            jaggedArray[0] = [1, 2, 3, 4];
-
+            Console.WriteLine(i);
         }
     }
 }
--- csharp_lembretes/Program.cs
+++ csharp_lembretes/Program.cs
@@ -8,3 +8,8 @@ using csharp_lembretes.Estruturas_de_controle;
 // Repeticao.For0toNum(20);
 // Repeticao.For_i_j(4);
 */
+
+using  csharp_lembretes.Colecoes;
+//Arrays.RedimencionarArray([ 1,2]);
+int[] array = [1, 2, 3];
+Arrays.CopiarArray(array);
