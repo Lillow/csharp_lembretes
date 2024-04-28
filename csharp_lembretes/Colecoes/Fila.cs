d3833afeeb1fd67ae2576e1c74fd7c32050bc945
Danillo <danillordm@gmail.com>
2024-04-28 20:50:16 -0300

    Adiciona Fila.cs

---
+++ csharp_lembretes/Colecoes/Fila.cs
@@ -0,0 +1,29 @@
+﻿namespace csharp_lembretes.Colecoes;
+
+internal class Fila
+{
+    public Fila(int elementos)
+    {
+        for (int i = 0; i < elementos; i++)
+        {
+            fila.Enqueue(i + 1);
+        }
+    }
+    
+    public  Queue<int> fila = new();
+
+    public  void MostrarElemtos()
+    {
+        foreach (var elemento in fila)
+        {
+            Console.WriteLine(elemento);
+        }
+    }
+
+    public void RemoverElemto()
+    {
+        fila.Dequeue();
+    }
+
+
+}
--- csharp_lembretes/Program.cs
+++ csharp_lembretes/Program.cs
@@ -2,66 +2,18 @@
 𝔸𝕢𝕦𝕚 𝕒𝕔𝕠𝕟𝕥𝕖𝕔𝕖𝕞 𝕠𝕤 𝕥𝕖𝕤𝕥𝕖𝕤
 *************************/
 
+using csharp_lembretes.Colecoes;
 
+Fila fila = new(10);
 
-/*
-using  csharp_lembretes.Colecoes;
-//Arrays.RedimencionarArray([ 1,2]);
-int[] array = [1, 2, 3];
-Arrays.CopiarArray(array);
-*/
+fila.RemoverElemto();
+fila.MostrarElemtos();
+Console.WriteLine("------------------------------------");
 
-//using csharp_lembretes.Colecoes;
+fila.RemoverElemto();
+fila.MostrarElemtos();
+Console.WriteLine("------------------------------------");
 
-//Listas.DebugLista();
-
-
-
-
-
-//using csharp_lembretes.Classes_objetos_heranca_polimorfismo;
-//ConstrutorEpropriedades objeot = new(-1);
-
-//using csharp_lembretes.Classes_objetos_heranca_polimorfismo;
-
-//ModificadoresDeAcesso mod =  new ModificadoresDeAcesso();
-
-
-//using csharp_lembretes.Variaveis_tipos_de_dados;
-
-//FormatacoesReais formata = new FormatacoesReais(.234f);
-//Console.WriteLine(formata.Dolar);
-//Console.WriteLine(formata.Real);
-//Console.WriteLine(formata.Casas);
-//Console.WriteLine(formata.Porcento);
-//Console.WriteLine(formata.FormatarPersonalizado);
-
-
-//using csharp_lembretes.Estruturas_de_controle;
-
-//// Repeticao.For0toNum(20);
-//// Repeticao.For_i_j(4);
-//*/
-
-//using csharp_lembretes.Variaveis_tipos_de_dados;
-//DataHora data = new(DataHora.AgoraMais5dias);
-//Console.WriteLine(data.DataString1);
-//Console.WriteLine(data.DataString2);
-//Console.WriteLine(data.DataCurta);
-//Console.WriteLine(data.HoraCurta);
-
-//Console.WriteLine(DataHora.StringParaData("2022-13-17 18:00"));
-
-
-
-//using csharp_lembretes.Manipulacao_de_arquivos;
-//Arquivo.Criar("arquvo.txt", "Mussum Ipsum, cacilds vidis litro abertis.  Eu nunca mais boto a boca num copo de cachaça, agora eu só uso canudis! Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Paisis, filhis, espiritis santis. Suco de cevadiss deixa as pessoas mais interessantis.\r\n\r\nNegão é teu passadis, eu sou faxa pretis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Viva Forevis aptent taciti sociosqu ad litora torquent. Nulla id gravida magna, ut semper sapien.\r\n\r\nPosuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. A ordem dos tratores não altera o pão duris. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. Copo furadis é disculpa de bebadis, arcu quam euismod magna.");
-//Arquivo.LerTodoTexto("arquivo.txt");
-//Arquivo.LerTodasLinhas("arquivo.txt");
-
-
-using csharp_lembretes.Tratamento_de_excecoes;
-
-//Tratamento.LerArquivo("awd/arquivao.txt");
-
-Tratamento.Metodo1();
+fila.RemoverElemto();
+fila.MostrarElemtos();
+Console.WriteLine("------------------------------------");
