6866a6ade1cd912435cd24fd1da555077f7be912
Danillo <danillordm@gmail.com>
2024-04-28 20:13:00 -0300

    Adiciona informações Manipulacao_de_arquivos

--- csharp_lembretes/Manipulacao_de_arquivos/Arquivo.cs
+++ csharp_lembretes/Manipulacao_de_arquivos/Arquivo.cs
@@ -9,4 +9,19 @@ internal class Arquivo
         File.WriteAllText(nomeDoArquivo, conteudo); //Criar arquivo
         Console.WriteLine($"O arquivo Json foi criado com sucesso! {Path.GetFullPath(nomeDoArquivo)}");
     }
+
+    public static void LerTodoTexto(string nome) 
+    {
+        string linhas = File.ReadAllText(nome);
+        Console.WriteLine(linhas);
+    }
+
+    public static void LerTodasLinhas(string nome)
+    {
+        string[] linhas = File.ReadAllLines(nome);
+        foreach(string line in linhas)
+        {
+            Console.WriteLine(line.Trim());
+        }
+    }
 }
