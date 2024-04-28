b9315631f27f9f90c1cc031a4283f2284c632617
Danillo <danillordm@gmail.com>
2024-04-28 20:13:31 -0300

    Adiciona informaçõe Tratamento_de_excecoes

--- csharp_lembretes/Program.cs
+++ csharp_lembretes/Program.cs
@@ -37,18 +37,31 @@ Arrays.CopiarArray(array);
 //Console.WriteLine(formata.FormatarPersonalizado);
 
 
-/**
-using csharp_lembretes.Estruturas_de_controle;
+//using csharp_lembretes.Estruturas_de_controle;
 
-// Repeticao.For0toNum(20);
-// Repeticao.For_i_j(4);
-*/
+//// Repeticao.For0toNum(20);
+//// Repeticao.For_i_j(4);
+//*/
 
-using csharp_lembretes.Variaveis_tipos_de_dados;
-DataHora data = new(DataHora.AgoraMais5dias);
-Console.WriteLine(data.DataString1);
-Console.WriteLine(data.DataString2);
-Console.WriteLine(data.DataCurta);
-Console.WriteLine(data.HoraCurta);
+//using csharp_lembretes.Variaveis_tipos_de_dados;
+//DataHora data = new(DataHora.AgoraMais5dias);
+//Console.WriteLine(data.DataString1);
+//Console.WriteLine(data.DataString2);
+//Console.WriteLine(data.DataCurta);
+//Console.WriteLine(data.HoraCurta);
 
-Console.WriteLine(DataHora.StringParaData("2022-13-17 18:00"));
+//Console.WriteLine(DataHora.StringParaData("2022-13-17 18:00"));
+
+
+
+//using csharp_lembretes.Manipulacao_de_arquivos;
+//Arquivo.Criar("arquvo.txt", "Mussum Ipsum, cacilds vidis litro abertis.  Eu nunca mais boto a boca num copo de cachaça, agora eu só uso canudis! Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Paisis, filhis, espiritis santis. Suco de cevadiss deixa as pessoas mais interessantis.\r\n\r\nNegão é teu passadis, eu sou faxa pretis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Viva Forevis aptent taciti sociosqu ad litora torquent. Nulla id gravida magna, ut semper sapien.\r\n\r\nPosuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. A ordem dos tratores não altera o pão duris. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. Copo furadis é disculpa de bebadis, arcu quam euismod magna.");
+//Arquivo.LerTodoTexto("arquivo.txt");
+//Arquivo.LerTodasLinhas("arquivo.txt");
+
+
+using csharp_lembretes.Tratamento_de_excecoes;
+
+//Tratamento.LerArquivo("awd/arquivao.txt");
+
+Tratamento.Metodo1();
--- csharp_lembretes/Tratamento_de_excecoes/Tratamento.cs
+++ csharp_lembretes/Tratamento_de_excecoes/Tratamento.cs
@@ -1,4 +1,6 @@
-﻿namespace csharp_lembretes.Tratamento_de_excecoes;
+﻿using csharp_lembretes.Manipulacao_de_arquivos;
+
+namespace csharp_lembretes.Tratamento_de_excecoes;
 
 internal class Tratamento
 {
@@ -14,5 +16,51 @@ internal class Tratamento
             // Se cair na excessão
             Console.WriteLine($"Temos um problema: {ex.Message}");
         }
+    }
+
+    public static void LerArquivo(string caminho) 
+    {
+        try
+        {
+            Arquivo.LerTodasLinhas(caminho);
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Arquivo não encontrado");
+        }
+        catch(DirectoryNotFoundException) 
+        {
+            Console.WriteLine($"Pasta não encontrada.");
+        }
+        finally
+        {
+            Console.WriteLine("Sempre é executado");
+        }
+    }
+
+    public static void Metodo1()
+    {
+        try
+        {
+        Metodo2();
+
+        }
+        catch
+        {
+            Console.WriteLine("Exceção tratada");
+        }
+    }
+
+    private static void Metodo2()
+    {
+        Metodo3();
+    }
+    private static void Metodo3()
+    {
+        Metodo4();
+    }
+    private static void Metodo4()
+    {
+        throw new Exception("Ocorreu uma exceção!");
     }
 }
