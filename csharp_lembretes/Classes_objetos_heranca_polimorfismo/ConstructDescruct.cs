6db5aae41f6a1a673481fa73595c21b1d5d7b7ca
Danillo <danillordm@gmail.com>
2024-04-29 10:29:20 -0300

    Adiciona ConstructDescruct.cs

---
+++ csharp_lembretes/Classes_objetos_heranca_polimorfismo/ConstructDescruct.cs
@@ -0,0 +1,25 @@
+﻿namespace csharp_lembretes.Classes_objetos_heranca_polimorfismo;
+
+/*Exemplo de uso de Destruct
+ConstructDestruct obj = new("ObjetoTeste", 42);
+var (nome, id) = obj;
+Console.WriteLine(nome + "_" + id);
+*/
+
+internal class ConstructDestruct
+{
+    public ConstructDestruct(string name, int id)
+    {
+        NomeClasse = name;
+        IdClasse = id;
+    }
+
+    public void Deconstruct(out string name, out int id) 
+    {
+        name = NomeClasse;
+        id = IdClasse;
+    }
+
+    public string NomeClasse { get; set; }
+    public int IdClasse { get; set; }
+}
